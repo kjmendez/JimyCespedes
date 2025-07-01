fbe8a3dd015ec8d32317e83a3b36012dc7129a15
jimy <ccespedes@prevision.com.bo>
2025-07-01 01:04:03 -0400

    Correccion configuracion postgres

--- src/database/config.js
+++ src/database/config.js
@@ -7,8 +7,8 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: process.env.DB_PORT || 3306,
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432,
     logging: false,
   }
 );
